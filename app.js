1a735967c5bff52f99807477bc5561058df94010
kiarash.shamaii <kiarash.shamaii@gmail.com>
2020-08-10 19:33:29 +0430

    fibonachi homework4 web3-Mr.kazemiRaz

---
+++ app.js
@@ -0,0 +1,20 @@
+var n = prompt('enter the length of yopr fibonachi needs:')
+ function fibonachi (n) 
+{
+  if(n<0){
+	 n=prompt('you must choose positive number');
+	 fibonachi(n);
+  }
+  else if  (n===1) 
+  {
+    return [0, 1];
+  } 
+  else 
+  {
+    var s = fibonachi(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+var series = fibonachi(5);
+alert(series);
